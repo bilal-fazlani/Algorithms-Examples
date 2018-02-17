9233742c812db057d95ac1b99782d7379c3cbb80
bilal-fazlani <bilal.m.fazlani@gmail.com>
2018-02-18 01:29:47 +0530

    Added algo to find volume of cylinder

---
+++ Cylinder.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms
+{
+    public class Cylinder
+    {
+        public double Radius { get; }
+        public double Height { get; }
+
+        public Cylinder(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+
+        public double GetVolume()
+        {
+            /*
+             * SET volume = pie * r^2 * h
+             */
+            return System.Math.PI * System.Math.Pow(Radius, 2D) * Height;
+        }
+    }
+}
---
+++ Tests/CylinderTests.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class CylinderTests
+    {
+        [Theory]
+        [InlineData(2D, 4D, 50.265482457436692)]
+        public void ShouldReturnVolumeOfCylinder(double radius, double height, double expectedVolume)
+        {
+            Cylinder cylinder = new Cylinder(radius, height);
+            cylinder.GetVolume().Should().Be(expectedVolume);
+        }
+    }
+}
