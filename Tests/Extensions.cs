f8a86724dd4145bc5d4446a7add3d797f024986b
bilal-fazlani <bilal.m.fazlani@gmail.com>
2018-02-18 16:42:21 +0530

    fix extension method for printing collections

--- Tests/Extensions.cs
+++ Tests/Extensions.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 
 namespace Tests
 {
     public static class Extensions
     {
-        public static string ToCommaSeparatedString(this IEnumerable enumerable)
+        public static string ToCommaSeparatedString(this IEnumerable<int> enumerable)
         {
             return string.Join(", ", enumerable);
         }
