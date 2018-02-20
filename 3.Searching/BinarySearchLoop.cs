ff7c862a7f5498b6d3bbcfaa19c44899d951cd41
bilal-fazlani <bilal.m.fazlani@gmail.com>
2018-02-20 23:09:53 +0530

    Added binary search with loop approach

---
+++ 3.Searching/BinarySearchLoop.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms._3.Searching
+{
+    public class BinarySearchLoop : ISearchingAlgorithm
+    {
+        public int Search(int[] values, int search)
+        {
+            int left = 0, right = values.Length-1;
+
+            do
+            {
+                int middle = (left + right) / 2;
+
+                if (values[middle] == search)
+                {
+                    return middle;
+                }
+
+                if (values[middle] > search)
+                {
+                    right = middle -1;
+                }
+                
+                if(values[middle] < search)
+                {
+                    left = middle + 1;
+                }
+            } while (right >= left);
+
+            return -1;
+        }
+    }
+}
---
+++ Tests/3.Searching/BinarySearchLoop.cs
@@ -0,0 +1,12 @@
+﻿using Algorithms._3.Searching;
+using Xunit.Abstractions;
+
+namespace Tests._3.Searching
+{
+    public class BinarySearchLoopTest : AbstractSearchingTest<BinarySearchLoop>
+    {
+        public BinarySearchLoopTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+    }
+}
