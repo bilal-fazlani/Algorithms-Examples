ea39e85ce6467468e3b96aebf2b46205958569c2
bilal-fazlani <bilal.m.fazlani@gmail.com>
2018-02-20 13:08:25 +0530

    Added selection sort

---
+++ 2.Sorting/SelectionSort.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Algorithms._2.Sorting
+{
+    public class SelectionSort : ISortingAlgorithm
+    {
+        public void SortAscending(int[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                for (int j = i+1; j < values.Length; j++)
+                {
+                    if (values[i] > values[j])
+                    {
+                        values.SwapValues(i, j);
+                    }
+                }
+            }
+        }
+
+        public void SortDescending(int[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                for (int j = i + 1; j < values.Length; j++)
+                {
+                    if (values[i] < values[j])
+                    {
+                        values.SwapValues(i, j);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tests/2.Sorting/SelectionSortTest.cs
@@ -0,0 +1,12 @@
+﻿using Algorithms._2.Sorting;
+using Xunit.Abstractions;
+
+namespace Tests._2.Sorting
+{
+    public class SelectionSortTest : AbstractSortingTest<SelectionSort>
+    {
+        public SelectionSortTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+    }
+}
