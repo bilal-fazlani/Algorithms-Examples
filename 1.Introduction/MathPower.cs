d39842f40557d0232d32f5c8336b05b388e8f243
bilal-fazlani <bilal.m.fazlani@gmail.com>
2018-02-21 18:33:49 +0530

    Refactor power function

--- 1.Introduction/MathPower.cs
+++ 1.Introduction/MathPower.cs
@@ -1,37 +1,9 @@
 ﻿namespace Algorithms._1.Introduction
 {
-    public class Math
+    public class PowerCalculator
     {
-        public int PowerV1(int baseNumber, int exponent)
+        public int Calculate(int baseNumber, int exponent)
         {
-            /*
-             * PSEUDOCODE
-             *
-             * SET result = 1
-             * WHILE i < exponent
-             *     result = result * base
-             *     exponent--
-             */
-
-            int result = 1;
-            while (exponent > 0)
-            {
-                result = result * baseNumber;
-                exponent--;
-            }
-
-            return result;
-        }
-
-        public int PowerV2(int baseNumber, int exponent)
-        {
-            /*
-             * SET result = 1
-             * FOR i : 0 -> exponent
-             *     SET result = result * baseNumber
-             *     i++
-             */
-
             int result = 1;
             for (int i = 0; i < exponent; i++)
             {
--- Tests/1.Introduction/MathPowerTest.cs
+++ Tests/1.Introduction/MathPowerTest.cs
@@ -1,6 +1,7 @@
+using Algorithms._1.Introduction;
 using FluentAssertions;
 using Xunit;
-using Math = Algorithms._1.Introduction.Math;
+using Math = Algorithms._1.Introduction.PowerCalculator;
 
 namespace Tests._1.Introduction
 {
@@ -17,23 +18,8 @@ namespace Tests._1.Introduction
         [InlineData(1, 0, 1)]
         public void TestV2(int baseNumber, int exponent, int expectedResult)
         {
-            Math math = new Math();
-            math.PowerV2(baseNumber, exponent).Should().Be( expectedResult);
-        }
-        
-        [Theory]
-        [InlineData(1, 1, 1)]
-        [InlineData(2, 1, 2)]
-        [InlineData(2, 2, 4)]
-        [InlineData(2, 3, 8)]
-        [InlineData(2, 0, 1)]
-        [InlineData(0, 0, 1)]
-        [InlineData(0, 1, 0)]
-        [InlineData(1, 0, 1)]
-        public void TestV1(int baseNumber, int exponent, int expectedResult)
-        {
-            Math math = new Math();
-            math.PowerV1(baseNumber, exponent).Should().Be( expectedResult);
+            PowerCalculator math = new PowerCalculator();
+            math.Calculate(baseNumber, exponent).Should().Be( expectedResult);
         }
     }
 }
